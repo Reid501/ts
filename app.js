4aca5685b8eaa948c5e4a8ccde97f6be809ebd8b
reid501 <alexatrs@hotmail.co.uk>
2022-10-10 17:58:34 +0100

    unions aliases and functions

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-var person = {
-    name: "Alex",
-    age: 30,
-    hobbies: ["sports", "cooking"],
-    role: [2, "author"]
-};
-person.role.push("admin");
-person.role[1] = 10;
-var favoriteActivites;
-favoriteActivites = ["sports"];
-console.log(person);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby);
+function add(n1, n2) {
+    return n1 + n2;
 }
+function printResult(num) {
+    console.log("Result: " + num);
+}
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+printResult(add(5, 12));
+var combineValues;
+combineValues = add;
+console.log(combineValues(8, 8));
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
