0570e04564b81a0086dba6dcb62fcf174a073f73
reid501 <alexatrs@hotmail.co.uk>
2022-11-08 18:54:28 +0000

    type guards - discriminated unions and type casting

---
+++ AdancedTypes/dist/app.js
@@ -0,0 +1,59 @@
+"use strict";
+const e1 = {
+    name: "Max",
+    privileges: ["create-server"],
+    startDate: new Date(),
+};
+function add(a, b) {
+    if (typeof a === "string" || typeof b === "string") {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    console.log("Name: " + emp.name);
+    if ("privileges" in emp) {
+        console.log("Privileges: " + emp.privileges);
+    }
+    if ("startDate" in emp) {
+        console.log("Start Date: " + emp.startDate);
+    }
+}
+printEmployeeInformation(e1);
+class Car {
+    drive() {
+        console.log("driving...");
+    }
+}
+class Truck {
+    drive() {
+        console.log("driving a truck....");
+    }
+    loadCargo(amount) {
+        console.log("Loading cargo...", amount);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
+function moveAniaml(animal) {
+    let speed;
+    switch (animal.type) {
+        case "bird":
+            speed = animal.flyingSpeed;
+            break;
+        case "horse":
+            speed = animal.groundSpeed;
+    }
+    console.log("Moving at speed: ", speed);
+}
+moveAniaml({ type: "bird", flyingSpeed: 30 });
+const userInputElement = (document.getElementById("user-input"));
+userInputElement.value = "Hi there!";
---
+++ AdancedTypes/src/app.ts
@@ -0,0 +1,106 @@
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+  name: "Max",
+  privileges: ["create-server"],
+  startDate: new Date(),
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log("Name: " + emp.name);
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("Start Date: " + emp.startDate);
+  }
+}
+
+printEmployeeInformation(e1);
+
+class Car {
+  drive() {
+    console.log("driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("driving a truck....");
+  }
+
+  loadCargo(amount: number) {
+    console.log("Loading cargo...", amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
+
+interface Bird {
+  type: "bird";
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse";
+  groundSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAniaml(animal: Animal) {
+  let speed;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.groundSpeed;
+  }
+  console.log("Moving at speed: ", speed);
+}
+
+moveAniaml({ type: "bird", flyingSpeed: 30 });
+
+const userInputElement = <HTMLInputElement>(
+  document.getElementById("user-input")!
+);
+
+userInputElement.value = "Hi there!";
