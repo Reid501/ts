76fe571a34e2ce392ea43b3c4e3c0b7b23f4b124
reid501 <alexatrs@hotmail.co.uk>
2022-11-28 16:21:22 +0000

    Generics

--- Generics/dist/app.js
+++ Generics/dist/app.js
@@ -8,3 +8,46 @@
 // promise.then((data) => {
 //   data.split(" ");
 // });
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
+}
+const mergedObj = merge({ name: "Alex" }, { age: 30 });
+console.log(mergedObj.age);
+function countAndDescribe(element) {
+    let descriptionText = "Got no value.";
+    if (element.length === 1) {
+        descriptionText = `Got 1 element`;
+    }
+    else if (element.length > 1) {
+        descriptionText = `Got ${element.length} elements`;
+    }
+    return [element, descriptionText];
+}
+console.log(countAndDescribe(["sports", "cooking"]));
+function extractAndConvert(obj, key) {
+    return `Vale: ${obj[key]}`;
+}
+console.log(extractAndConvert({ name: "Alex" }, "name"));
+class DataStorage {
+    constructor() {
+        this.data = [];
+    }
+    addItem(item) {
+        this.data.push(item);
+    }
+    removeItem(item) {
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    getItems() {
+        return [...this.data];
+    }
+}
+function createCourseGoal(title, description, date) {
+    let courseGoal = {};
+    courseGoal.title = title;
+    courseGoal.description = description;
+    courseGoal.completeUntil = date;
+    return courseGoal;
+}
+const names = ["Alex", "Giv"];
+// names.push('Reid')
--- Generics/src/app.ts
+++ Generics/src/app.ts
@@ -9,3 +9,90 @@
 // promise.then((data) => {
 //   data.split(" ");
 // });
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: "Alex" }, { age: 30 });
+console.log(mergedObj.age);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value.";
+
+  if (element.length === 1) {
+    descriptionText = `Got 1 element`;
+  } else if (element.length > 1) {
+    descriptionText = `Got ${element.length} elements`;
+  }
+
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe(["sports", "cooking"]));
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return `Vale: ${obj[key]}`;
+}
+
+console.log(extractAndConvert({ name: "Alex" }, "name"));
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+// const textStorage = new DataStorage<string>();
+// textStorage.addItem("Alex");
+// textStorage.addItem("Giv");
+// textStorage.removeItem("Alex");
+// console.log(textStorage.getItems());
+
+// const numberStorage = new DataStorage<number>();
+
+// const objStorage = new DataStorage<object>();
+// objStorage.addItem({ name: "Alex" });
+// objStorage.addItem({ name: "Giv" });
+
+// objStorage.removeItem({ name: "Giv" });
+// console.log(objStorage.getItems());
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+
+  return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ["Alex", "Giv"];
+// names.push('Reid')
